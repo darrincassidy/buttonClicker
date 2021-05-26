f982909d505383234926154508618a97699a7182
darrincassidy <darrincassidy@gmail.com>
2021-05-26 01:17:26 -0400

    first commit

---
+++ buttonClicker.js
@@ -0,0 +1,11 @@
+function getFirstButton() {
+    alert("I come in peace from the red planet!");
+}
+
+function getSecondButton() {
+    alert("I come in peace from the white planet!");
+}
+
+function getRedButtons() {
+    alert("WE come in peace from the blue planet!");
+}
